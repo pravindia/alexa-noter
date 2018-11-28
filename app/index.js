555411828c5b4772f26173feab2ac89852b03ee5
pravindia <pravinda4hero@gmail.com.com>
2018-11-28 23:46:03 +0530

    init

---
+++ app/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const alexa = require('./alexa.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', (req, res) => {
+  alexa.handler(req.body, (response) => {
+    if (response === "Invalid application id!")
+      res.status(401).send(response);
+    else
+      res.send(response);
+  })
+});
+
+exports.listener = app;
